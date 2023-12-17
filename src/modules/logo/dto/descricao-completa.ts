8955edbf5791e3af2eabbd7f343734513b39fea8
Joao Marcos <55150938+jmlv-git@users.noreply.github.com>
2023-12-16 22:54:57 -0300

    Finzalizacao criar Logo

---
+++ src/modules/logo/dto/descricao-completa.ts
@@ -0,0 +1,8 @@
+export class DescricaoCompletaDto {
+    cliente: string
+    tipoOrganizacao: string
+    ramoAtuacao: string
+    diferenciais: string
+    cores?: string
+    elementos?: string
+}
--- src/modules/logo/logo.controller.ts
+++ src/modules/logo/logo.controller.ts
@@ -1,25 +1,32 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { LogoService } from './logo.service';
-import { CreateLogoDto } from './dto/create-logo.dto';
 import { UpdateLogoDto } from './dto/update-logo.dto';
 import ProcessadorConverterFormulario from './iProcessadorChainHandlers/ProcessadorConverterFormulario';
 import GerarDescricaoGpt from './iProcessadorChainHandlers/ProcessadorGerarDescricao';
+import { DescricaoCompletaDto } from './dto/descricao-completa';
+import { CreateLogoDto } from './dto/create-logo.dto';
 
 @Controller('logo')
 export class LogoController {
   constructor(private readonly logoService: LogoService) {}
 
   @Post()
-  async create(@Body() createLogoDto: CreateLogoDto) {
+  async create(@Body() data: DescricaoCompletaDto) {
     
     let handler_1 = new ProcessadorConverterFormulario ()
     handler_1.setNext(new GerarDescricaoGpt())
-    let stringFormulario = "ibama##protecao aos animais##preservação da natureza##verde##um macaco"
-    let prompt1 = await handler_1.handler(stringFormulario)
+    let stringFormulario = data.tipoOrganizacao+"##"+data.ramoAtuacao+"##"+data.diferenciais;
+    if (data.cores) {
+      stringFormulario=stringFormulario+"##"+data.cores;
+    }
+    if (data.elementos) {
+      stringFormulario=stringFormulario+"##"+data.elementos;
+    }
+    let img = await handler_1.handler(stringFormulario)
 
     const logoAux: CreateLogoDto = {
-      imagem:  prompt1,
-      loginCliente: "fefeberotec"
+      imagem:  img,
+      loginCliente: data.cliente
    }
     
     const logo = await this.logoService.cadastrarLogo(logoAux);
